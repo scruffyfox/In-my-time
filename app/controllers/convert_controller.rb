d71426b0e7e2a7efe73d3261be94fd6aad809151
Callum Taylor <callum@3sidedcube.com>
2012-12-04 17:56:09 +0000

    Added base settings

--- app/controllers/convert_controller.rb
+++ app/controllers/convert_controller.rb
@@ -1,5 +1,10 @@
 class ConvertController < ActionController::Base
-  def index
+  respond_to :html, :xml, :json
 
+  def index
+    respond_with(@switches) do |format|
+      format.html
+      format.json { render :none => true }
+    end
   end
 end
